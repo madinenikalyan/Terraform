9568ce0860f6084ca3b107d4d4b2b39b4a7146d0
madinenikalyan <kchakravarti10@gmail.com>
2022-09-10 23:49:59 +0530

    updated

--- Network/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  nat_subnet_ids = [module.vpc.public_subnet_ids[0],module.vpc.public_subnet_ids[1]]
-}
--- Network/nat.tf
+++ Network/nat.tf
@@ -1,28 +1,4 @@
-# resource "aws_eip" "eip_nat" {
-#   count = length(local.nat_subnet_ids)
-#   vpc = true
-#  tags = merge(
-#      {
-#       Name        = format("EIP-%s",local.nat_subnet_ids[count.index]),
-#      },
-#      var.tags
-#   )
-# }
 
-# resource "aws_nat_gateway" "nat_gateway" {
-  
-#   count = length(local.nat_subnet_ids)
-
-#   allocation_id = aws_eip.eip_nat[count.index].id
-#   subnet_id     = local.nat_subnet_ids[count.index]
-
-#    tags = merge(
-#      {
-#       Name        = format("NAT-%s",local.nat_subnet_ids[count.index]),
-#      },
-#      var.tags
-#   )
-# }
 
 module "nat01" {
     source = "./module/nat"
